bf9e7df1015168cbf32144d69e403f12c927c2e1
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-06-03 15:55:09 +0400

    fixed scrolling

--- js/site.js
+++ js/site.js
@@ -1,14 +1,16 @@
-
-	jQuery(document).ready(function($) {
-
-		    //$('#nav-main').scrollspy()
-		    
-		    // Localscrolling 
-    		//$('#nav-main, .brand').localScroll();
-     		//$('#news, .container').localScroll();
-     		$('#news, .container').localScroll();
-
+jQuery(document).ready(function($) {
+	$('.nav.navbar-nav.navbar-right li a').on('click', function(e) {
+		e.preventDefault();
+		var href = e.target.href;
+		href = href.split('#')[1];
+		var top = $('#' + href).closest('section').offset().top - 92;
+		window.scrollTo({
+			top: top,
+			behavior: "smooth"
+		});
 	});
+});
+
 	
 	
 
